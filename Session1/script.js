3cff3ddb1078d7d23d34a4a4b1c202d3ed15b69b
KRienecker7 <66344839+KRienecker7@users.noreply.github.com>
2020-06-30 18:58:40 -0700

    JavaScript

---
+++ Session1/script.js
@@ -0,0 +1,35 @@
+console.log("Executed");
+
+
+//Create a variable called carName, assign the value Volvo it on the console.
+
+var carName="Volvo";
+console.log(carName);
+//Create a variable called x, assing the value 50 to it and display it on the console.
+
+var x =50;
+console.log(x);
+
+let band="Metalica"; song= "Enter sandman";
+
+let name;
+
+name=band+ " : " + song;
+console.log(name.toUpperCase());
+console.log(name.length);
+
+let activity = "I am learnig JavaScript in this course";
+name=activity.split(" ");
+console.log(name);
+
+let interests = "programming, learning, music, volleyball, series";
+name = interests.split(" , ");
+console.log(name);
+
+name=activity.includes("JavaScript");
+console.log(name);
+
+email="ebbonai@gmail.com";
+name=email.includes("@")
+
+console.log(name);
---
+++ Session1/script2.js
@@ -0,0 +1,29 @@
+//arrays
+
+const months = new Array("Jan","Feb","Mar","Apr")
+
+months.push("May");// adds a new element at the end
+
+months.unshift("Month 0");// adds a new element at the beginning
+
+months[3]="March";
+
+console.table(months);
+
+alert(months);
+
+//alert(months);
+
+var numbers = [10,2,3,4,5,6];
+
+//sum the numbers
+
+var total = numbers[0]+numbers[1]+numbers[2]+numbers[3]+numbers[4]+numbers[5];
+
+//calculate average
+
+var average = total/6;
+
+//display alert();
+
+alert("the average is "+average);
