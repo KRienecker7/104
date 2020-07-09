618e3a02b478f11b49e8d0b770d689a0a7d9e048
KRienecker7 <66344839+KRienecker7@users.noreply.github.com>
2020-07-09 10:23:04 -0700

    Competency Report

--- cr/script.js
+++ cr/script.js
@@ -7,23 +7,20 @@ class Player{
 
     display=function(){
         console.log(this.name + " " + this.energy);
-        //display the energy on the info division
-        //enerngyAmount=function(){
         document.getElementById(this.id).innerHTML=`
-        <p> ${this.energy} is the Total Energy Remaining!!!</p>
+        <p>Energy:  ${this.energy} is the Total Energy Remaining!!!</p>
         `;
-        //    }
     }
     attack=function(opponent){
         document.getElementById("console").innerHTML=`
-            <p> ${this.name} is Attacking ${opponent.name}!!!</p>
+        <p> ${this.name} is Attacking ${opponent.name}!!!</p>
         `;
 
         let newEnergy=opponent.energy-20;
         opponent.energy=newEnergy;
         opponent.display();
 
-        if(opponent.energy==0){
+        if(opponent.energy<=0){
             document.getElementById("console").innerHTML=`
             <p> G A M E   O V E R!!!</p>
         `;
@@ -32,12 +29,12 @@ class Player{
     }
 }
 
-const Blazer = new Player("Blazer","Blazer", 100);
-const char2 = new Player("Crom","Crom", 120);
+const Blazer = new Player("player1","Blazer", 100);
+const Crom = new Player("player2","Crom", 120);
 
 
 //display();wrong
 
 Blazer.display();
-char2.display();
+Crom.display();
 
